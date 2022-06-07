b02e9ef3fff886c985eae78c55de48147284e201
Dobby-progam <dthweathertime@gmail.com>
2022-06-07 12:44:46 +0200

    UPDATED EVRY TING!

---
+++ contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.html' ;?>
+
+<body>
+<li id= main>Twitter <a href="https://twitter.com/dth14883706"><i class="fa-brands fa-twitter fa-2x">test</i><a></li>
+<li id=main> Did you found a bug or error? tell it me on Github <a href="https://github.com/Dobby-progam"><i class="fa-brands fa-github fa-2x">test</i><a> </li>
+<li id=main> Just want to hang out whit me? Join the Discord! <a href="https://discord.gg/KVSsXdRmFw"><i class="fa-brands fa-discord fa-2x">test</i><a> </li>
+<li id=main> You can E-mail me   <a href="mailto:dthweathertime@gmail.com"><i class="fa-solid fa-envelope fa-2x">test</i><a> </li>
+<li id=main> I post somtimes a video on youtube  <a href="https://www.youtube.com/channel/UCESZIx8ZvA5kf6PyyPLBZSQ"><i class="fa-brands fa-youtube fa-2x">test</i><a> </li>
+<li id=main> I stream on Twitch  <a href="https://www.twitch.tv/dobbie_streams"><i class="fa-brands fa-twitch fa-2x">test</i><a> </li>
+<?php include 'onder.html' ;?>
+</body>
+</html>
